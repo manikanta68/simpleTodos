9ad9b5a568b96bad053731dd824fc76f7f147b16
manikant68 <smanikanta515@gmail.com>
2024-03-11 14:20:11 +0000

    secondCommit

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -6,41 +6,57 @@ const initialTodosList = [
   {
     id: 1,
     title: 'Book the ticket for today evening',
+    edit: false,
+    check: false,
   },
   {
     id: 2,
     title: 'Rent the movie for tomorrow movie night',
+    edit: false,
+    check: false,
   },
   {
     id: 3,
     title: 'Confirm the slot for the yoga session tomorrow morning',
+    edit: false,
+    check: false,
   },
   {
     id: 4,
     title: 'Drop the parcel at Bloomingdale',
+    edit: false,
+    check: false,
   },
   {
     id: 5,
     title: 'Order fruits on Big Basket',
+    edit: false,
+    check: false,
   },
   {
     id: 6,
     title: 'Fix the production issue',
+    edit: false,
+    check: false,
   },
   {
     id: 7,
     title: 'Confirm my slot for Saturday Night',
+    edit: false,
+    check: false,
   },
   {
     id: 8,
     title: 'Get essentials for Sunday car wash',
+    edit: false,
+    check: false,
   },
 ]
 
 // Write your code here
 
 class SimpleTodos extends Component {
-  state = {todosList: initialTodosList}
+  state = {todosList: initialTodosList, inputValue: ''}
 
   onDelete = id => {
     const {todosList} = this.state
@@ -50,18 +66,112 @@ class SimpleTodos extends Component {
     this.setState({todosList: filteredList})
   }
 
-  render() {
+  onChangeInput = event => {
+    this.setState({inputValue: event.target.value})
+  }
+
+  onAddTodo = () => {
+    const {inputValue, todosList} = this.state
+    const searchWord = inputValue.split(' ')
+    const number = parseInt(searchWord[searchWord.length - 1])
+
+    if (typeof number === 'number') {
+      const newList = []
+      let i = 1
+      while (i <= number) {
+        const newTodo = {
+          id: todosList.length + i,
+          title: inputValue.slice(0, inputValue.length - 1),
+          edit: false,
+          check: false,
+        }
+        newList.push(newTodo)
+        i += 1
+      }
+      console.log(newList)
+      this.setState(prevState => ({
+        todosList: [...prevState.todosList, ...newList],
+        inputValue: '',
+      }))
+    } else {
+      const newTodo = {
+        id: todosList.length + 1,
+        title: inputValue,
+        edit: false,
+        check: false,
+      }
+      this.setState(prevState => ({
+        todosList: [...prevState.todosList, newTodo],
+        inputValue: '',
+      }))
+    }
+  }
+
+  editTextInput = id => {
     const {todosList} = this.state
+    const modifiedList = todosList.map(each => {
+      if (each.id === id) {
+        return {...each, edit: !each.edit}
+      }
+      return each
+    })
+    this.setState({todosList: modifiedList})
+  }
+
+  editSave = (value, id) => {
+    const {todosList} = this.state
+    const modifiedList = todosList.map(each => {
+      if (each.id === id) {
+        return {...each, title: value}
+      }
+      return each
+    })
+    this.setState({todosList: modifiedList})
+  }
+
+  checkFunction = (value, id) => {
+    const {todosList} = this.state
+    const modifiedList = todosList.map(each => {
+      if (each.id === id) {
+        return {...each, check: value}
+      }
+      return each
+    })
+    this.setState({todosList: modifiedList})
+  }
+
+  render() {
+    const {todosList, inputValue} = this.state
+
     return (
       <div className="bg-container">
         <div className="container">
           <h1 className="heading">Simple Todos</h1>
+          <div className="inputFiled">
+            <input
+              onChange={this.onChangeInput}
+              value={inputValue}
+              className="input"
+              type="text"
+            />
+            <button
+              onClick={this.onAddTodo}
+              className="addButton"
+              type="button"
+            >
+              Add
+            </button>
+          </div>
           <ul>
             {todosList.map(eachItem => (
               <TodoItem
                 eachTodoItem={eachItem}
                 key={eachItem.id}
                 onDelete={this.onDelete}
+                editTextInput={this.editTextInput}
+                editSave={this.editSave}
+                editSaveInput
+                checkFunction={this.checkFunction}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -2,18 +2,52 @@
 import './index.css'
 
 const TodoItem = props => {
-  const {eachTodoItem, onDelete} = props
-  const {title, id} = eachTodoItem
+  const {eachTodoItem, onDelete, editTextInput, editSave, checkFunction} = props
+  const {title, id, edit, check} = eachTodoItem
   const onClickChange = () => {
     onDelete(id)
   }
+
+  const onEditFunction = () => {
+    editTextInput(id)
+  }
+
+  const onSaveChange = event => {
+    editSave(event.target.value, id)
+  }
+
+  const onCheck = event => {
+    checkFunction(event.target.checked, id)
+  }
+
   return (
     <li className="list-item">
       <div className="todo-container">
-        <p className="para">{title}</p>
-        <button onClick={onClickChange} className="button" type="button">
-          Delete
-        </button>
+        <div className="checkWithPara">
+          <input onChange={onCheck} type="checkbox" />
+          {edit ? (
+            <input
+              onChange={onSaveChange}
+              className="editableTextInput"
+              value={title}
+            />
+          ) : (
+            <p className={check ? 'para textDecoration' : 'para'}>{title}</p>
+          )}
+        </div>
+
+        <div>
+          <button
+            onClick={onEditFunction}
+            className={edit ? 'editAndSaveButton extra' : 'editAndSaveButton'}
+            type="button"
+          >
+            {edit ? 'Save' : 'Edit'}
+          </button>
+          <button onClick={onClickChange} className="button" type="button">
+            Delete
+          </button>
+        </div>
       </div>
     </li>
   )
